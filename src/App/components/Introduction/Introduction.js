fe4827cb102bc0ab414ee332ea352bd8529c097a
Nicholas Piano <nicholas.d.piano@gmail.com>
2020-01-21 07:25:25 +0100

    Update introduction and usage

--- src/App/components/Introduction/Introduction.js
+++ src/App/components/Introduction/Introduction.js
@@ -1,14 +1,40 @@
 
 import React, { Fragment } from 'react';
 import ReactMarkdown from 'react-markdown';
+import {
+  MdArrowBack,
+  MdArrowDownward,
+  MdArrowForward,
+  MdArrowUpward,
+  MdSubdirectoryArrowLeft,
+} from "react-icons/md";
 
 import introduction from './introduction.json';
 
 const Introduction = () => (
   <Fragment>
     <ReactMarkdown source={introduction.header} />
-    <ReactMarkdown source={introduction.blurb} />
-    <ReactMarkdown source={introduction.encodings} />
+    <ReactMarkdown source={introduction.headerText} />
+    <ReactMarkdown source={introduction.credits} />
+    <ReactMarkdown source={introduction.creditsText} />
+    <ReactMarkdown source={introduction.api} />
+    <ReactMarkdown source={introduction.apiText} />
+    <ReactMarkdown source={introduction.usage} />
+    <ReactMarkdown source={introduction.usageText} />
+    <p>While textbox is focussed: </p>
+    <p>
+      <MdArrowBack />
+      <MdArrowDownward />
+      <MdArrowForward />
+      <MdArrowUpward />
+      &nbsp;
+      Use arrow keys to move grey highlight box
+    </p>
+    <p>
+      <MdSubdirectoryArrowLeft />
+      &nbsp;
+      Use enter key to complete grey highlighted character and trigger transliteration
+    </p>
   </Fragment>
 );
 
